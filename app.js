d188f08c31fb5405cd1ad36609da7d4e507ea5ce
Francis Luigie Quenano <flquenano@gmail.com>
2020-05-19 11:52:12 -0500

    adjustment #2

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
+app.use("/api/v1/user", user_routes);
+app.use("/api/v1/posts", post_routes);
+app.use("/api/v1/upload", upload_routes);
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 } else {
@@ -31,10 +35,6 @@ if (process.env.NODE_ENV === "development") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
-app.use("/ap/v1/user", user_routes);
-app.use("/ap/v1/posts", post_routes);
-app.use("/ap/v1/upload", upload_routes);
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("client/build"));
